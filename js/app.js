5c55a7006cc9483b89c7838d013cdd353d5a7a25
Estanislao Cairoli <estanislaocairoli@gmail.com>
2024-03-18 17:49:27 -0300

    se agrego array y funciones de filtrado

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // Solicitud de información
 alert("Bienvenido a DC gimnasios");
-let nombre = prompt("Ingrese su nombre por favor:");
-let ciudad = prompt("Ingrese la ciudad donde quiere asistir a nuestros gimnasios");
+let nombre = prompt("Ingrese su nombre por favor:").toUpperCase();
+let ciudad = prompt("Ingrese la ciudad donde quiere asistir a nuestros gimnasios").toUpperCase();
 let dias = Number(prompt("Hola " + nombre + ", ingresa cantidad de días (1-6 días) que asistiras al gimnasio:"));
 let precio = 10000; // Declaración de precio mensual mínimo
 
@@ -15,29 +15,60 @@ function precios(){
     }else if((dias <= 4)&&(dias >2)){
         let mensualidad = precio * 1.25;
         alert(nombre + ", tu mensualidad será de $" + mensualidad );
-    }else if((dias >= 2)&&(dias >=1)){
+    }else if((dias <= 2)&&(dias >=1)){
         alert(nombre + ", tu mensualidad será de $" + precio );
 }else{
     alert(nombre + " la cantidad de días ingresado es erroneo, por favor ingresá un número entre 1 y 6. Muchas gracias");
 }
 }
 precios();
+// funciones vinculadas a la busqueda de sucursales
+
+function mostrarSucursales(sucursales){
+    sucursales.forEach(sucursal => {
+        alert(nombre + ', nuestro gimnasio mas cercano en ' + sucursal.localidad + ' esta en ' + sucursal.domicilio + ' y el telefono es ' + sucursal.telefono)
+        //(sucursal.localidad + ' - ' + sucursal.domicilio + ' - ' + sucursal.telefono)
+    });
+}
+
+function filtrarLocalidad(sucursal){
+    if(ciudad){
+        return sucursal.localidad === ciudad
+    }
+    return sucursal;
+}
+
+function filtrarSucursal(){
+    const resultado = sucursales.filter(filtrarLocalidad);
+    if(resultado.length>0){
+        mostrarSucursales(resultado)
+    }else{
+        console.error('no existe la localidad')
+    }
+}
+
+filtrarSucursal();
+filtrarLocalidad(ciudad);
+
+
+
 
 //Ciclo para aportar las direcciones cercanas
-while(ciudad != "ESC"){
-    switch(ciudad){
-        case "La Plata":
-           alert(nombre + ", nuestro gimnasio mas cerca esta en calle 50 n°1326");
-            break;
-        case "Quilmes":
-            alert(nombre + ", nuestro gimnasio mas cerca esta en Av. H. Yrigoyen n°526");
-            break;
-        case "CABA":
-            alert(nombre + ", nuestros gimnasio en CABA son: Belgrano (Cramer n°1953), Recoleta (Laprida n°2147), Caballito (Formosa n°259)");
-            break;
-        default:
-            alert(nombre + ", aun no tenemos sucursales cerca de tu casa.");
-            break;
-    }
-    alert("Gracias por confiar en DC gimnasios");
-}
+// while(ciudad != "ESC"){
+//     switch(ciudad){
+//         case "LA PLATA":
+//            alert(nombre + ", nuestro gimnasio mas cerca esta en " sucursal.localidad + 'esta en ' + sucursal.domicilio + ' y el telefono es ' + sucursal.telefono);
+//             break;
+//         case "QUILMES":
+//             alert(nombre + ", nuestro gimnasio mas cerca esta en Av. H. Yrigoyen n°526");
+//             break;
+//         case "CABA":
+//             alert(nombre + ", nuestros gimnasio en CABA son: Belgrano (Cramer n°1953), Recoleta (Laprida n°2147), Caballito (Formosa n°259)");
+//             break;
+//         default:
+//             alert(nombre + ", aun no tenemos sucursales cerca de tu casa.");
+//             break;
+//     }
+//     alert("Gracias por confiar en DC gimnasios");
+// }
+
---
+++ js/sucursales.js
@@ -0,0 +1,32 @@
+// Array de sucursales
+let sucursales = [
+    {
+        localidad: "LA PLATA", 
+        domicilio: "calle 50 n°1326",
+        telefono: "+5492216582212"
+    },
+    {
+        localidad: "QUILMES", 
+        domicilio: "Av. H. Yrigoyen n°526",
+        telefono: "+5491145657898"
+    },
+    {
+        localidad: "BELGRANO", 
+        domicilio: "Cramer n°1953",
+        telefono: "+5491136962585"
+    },
+    {
+        localidad: "RECOLETA", 
+        domicilio: "Laprida n°2147",
+        telefono: "+5491174148528"
+    },
+    {
+        localidad: "CABALLITO", 
+        domicilio: "Formosa n°259",
+        telefono: "+5491198563254"
+    },
+
+]
+for (const sucursal of sucursales){
+    console.log(sucursal)
+}
